369afc21a073d34456c19a33d7542ad1cd34d7db
Nathan-Wen <36411251+Nathan-Win-Wen@users.noreply.github.com>
2018-03-23 09:17:43 -0400

    Update Library.java

--- src/Library.java
+++ src/Library.java
@@ -2,7 +2,10 @@
  * Library class
  */
 public class Library {
-
+    String name;
+    Address location;
+    Book[] books;
+    Member[] members;
 
     /**
      * addBook(Book) : void
@@ -14,8 +17,16 @@ public class Library {
      * @param book
      *          A new book object to add to the books list
      */
-
-
+       
+       public void String[] addBook (book) {
+           String[] newBooks = new String[this.books.length + 1]
+           for (int i = 0; i < this.books.length; i ++) {
+            newBooks[i] = this.books[i];
+           }
+           
+           newBooks[i] = book;
+           
+       }
 
 
 
@@ -30,6 +41,15 @@ public class Library {
      * @param member
      *          A new member object to add to the members list
      */
+     public void String[] addMember (member) {
+           String[] newMember = new String[this.members.length + 1]
+           for (int i = 0; i < this.members.length; i ++) {
+            newMember[i] = this.members[i];
+           }
+      
+           newMember[i] = member;
+           
+       }
 
 
 
